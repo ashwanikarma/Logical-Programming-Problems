5255ef4d36157403ba4ca8cb0f75947e23a56305
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-10 02:55:40 +0530

    Added Perfect Number problem

--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -4,20 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fibonacci Series");
-            Console.WriteLine("Enter the number of terms you want to print : ");
-            int length = Convert.ToInt32(Console.ReadLine());
-            int third = 0, first = 0, second = 1, temp;
-            Console.Write(first+" "+second+" ");
-            for (int i = 2; i < length; i++)
+            Console.WriteLine("Perfect Number");
+            Console.Write("Enter the number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result=0;
+            for (int i = 1; i < number ; i++)
             {
-                third = first + second;
-                temp = second;
-                Console.Write(third+" ");
-                second = third;
-                first = temp;
+                if ( number % i == 0 )
+                {
+                    Console.Write(i+"+");
+                    result += i;
+                }
             }
+            Console.WriteLine("\b : {0}",result);
 
+            if (number == result)
+            {
+                Console.WriteLine("It's a Perfect Number.");
+            }
+            else
+            {
+                Console.WriteLine("It's not a Perfect Number.");
+            }
         }
     }
 }
