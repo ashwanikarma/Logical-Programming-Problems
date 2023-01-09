60c82b80843e2a8c4b088581b549c6b45a3b02c5
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-10 02:44:29 +0530

    Added fibbonacci series problem

---
+++ Logical Programs/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci Series");
+            Console.WriteLine("Enter the number of terms you want to print : ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int third = 0, first = 0, second = 1, temp;
+            Console.Write(first+" "+second+" ");
+            for (int i = 2; i < length; i++)
+            {
+                third = first + second;
+                temp = second;
+                Console.Write(third+" ");
+                second = third;
+                first = temp;
+            }
+
+        }
+    }
+}
